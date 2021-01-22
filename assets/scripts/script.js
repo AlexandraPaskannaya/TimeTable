58f02b4878b897c785bf37a3d2f85859933f5fd0
AlexandraPaskannaya <paskannaya1998@yandex.by>
2021-01-22 16:06:05 +0300

    second part

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,49 +1,38 @@
 let show_button = document.querySelector('button[data-action="show"]');
 let hide_button = document.querySelector('button[data-action="hide"]');
 let list_container = document.querySelector('.list-container');
-//let check_li = document.querySelectorAll("ul li");
-//let checkboxes = document.querySelectorAll("ul input[type='checkbox']:checked");
+let check_li = document.querySelectorAll("ul li");
 
 document.addEventListener('click', function(event) {
   let action = event.target.dataset.action;
   if (!action) return;
 
   this.show = function() {
-      console.log('show function');
-      show_button.classList.remove('active');
-      hide_button.classList.add('active');
-      list_container.classList.remove('hide');
-  }
+    console.log('show function');
+
+    check_li.forEach((e) =>{
+      if (e.firstElementChild.checked == true){
+        e.classList.remove('checked');
+        show_button.classList.remove('active');
+        hide_button.classList.add('active');
+      }
+  });
+}
 
   this.hide = function() {
     console.log('hide function');
-    hide_button.classList.remove('active');
-    show_button.classList.add('active');
-    list_container.classList.add('hide');
-    }
 
-  /*for(let i = 0; i < checkboxes.length; i++) {  
-    if(checkboxes[i] === true) {
-      checkboxes.classList.remove('checked')
-    } else {
-      checkboxes.classList.add('checked');
+    check_li.forEach((e) =>{
+      if (e.firstElementChild.checked == true){
+        e.classList.add('checked')
+        hide_button.classList.remove('active');
+        show_button.classList.add('active');
       }
- };*/
+    })
+  };
 
   if(event.target.classList.contains('active')) {
     this[action]();
   }
  });
  
- 
- /*check_li.forEach(c => c.addEventListener('change', e => {
-  hide_button.classList.toggle('checked', !document.querySelectorAll('.check:not(:checked)').length)
-}));*/
-
-/*
-checkboxes.addEventListener('click', function(ev) {
-  if (ev.target.tagName === true) {
-    ev.target.classList.add('checked');
-  }
-}, false);
-*/
