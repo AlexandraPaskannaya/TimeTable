203556a9c30ef33c024cbf356189eb1a8996f6c0
AlexandraPaskannaya <paskannaya1998@yandex.by>
2021-02-12 16:53:10 +0300

    with localStorage

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,14 @@
 let show_button = document.querySelector('button[data-action="show"]');
 let hide_button = document.querySelector('button[data-action="hide"]');
+let addBtn_button = document.querySelector('button[data-action="addBtn"]');
 let list_container = document.querySelector('.list-container');
-let check_li = document.querySelectorAll("ul li");
+let check = document.querySelectorAll("#li");
+let add_hide = document.querySelector('.add_container');
+
+let input = document.querySelector('#input');
+let add_plus = document.querySelector('#add');
+let result = document.getElementById('result');
+
 
 document.addEventListener('click', function(event) {
   let action = event.target.dataset.action;
@@ -10,7 +17,7 @@ document.addEventListener('click', function(event) {
   this.show = function() {
     console.log('show function');
 
-    check_li.forEach((e) =>{
+    check.forEach((e) =>{
       if (e.firstElementChild.checked == true){
         e.classList.remove('checked');
         show_button.classList.remove('active');
@@ -22,7 +29,7 @@ document.addEventListener('click', function(event) {
   this.hide = function() {
     console.log('hide function');
 
-    check_li.forEach((e) =>{
+    check.forEach((e) =>{
       if (e.firstElementChild.checked == true){
         e.classList.add('checked')
         hide_button.classList.remove('active');
@@ -30,9 +37,70 @@ document.addEventListener('click', function(event) {
       }
     })
   };
+/*
+  this.addBtn = function() {
+    console.log(event.target);
+
+    addBtn_button.classList.toggle('active');
+    addBtn_button.classList.remove('hide_input');
+    addBtn_button.classList.add('show_input');
+  }*/
 
   if(event.target.classList.contains('active')) {
     this[action]();
   }
  });
+
+ // localStorage
+ let todos;
  
+ function tolacal() {
+  todos = result.innerHTML;
+  localStorage.setItem('todos', todos);
+ }
+
+ // вешаем обработчик на кнопку "Добавить"
+
+addBtn_button.addEventListener("click", function() {
+   console.log('add button');
+ 
+   if (add_hide.classList.contains("hide_input")) {
+    add_hide.classList.toggle("show_input");
+  } 
+   this.classList.toggle("active");
+ })
+
+//вешаем обработчик на плюс в input 
+
+ add_plus.addEventListener('click', function() {
+  if(input.value === '') return;
+  
+  createDeleteElements(input.value);
+  input.value = '';
+  tolacal()
+ }) 
+
+// добавляем введенную задачу в конец списка ul
+
+ function createDeleteElements(value) {
+   console.log(value)
+
+  let liLast = document.createElement('li');
+  let input = document.createElement('input');
+  let label = document.createElement('label');
+
+  liLast.className = 'li';
+
+   input.type = 'checkbox';
+   liLast.prepend(input);
+
+   liLast.append(label);
+   label.textContent = ' ' + value;
+   
+   result.appendChild(liLast);
+   tolacal()
+ }
+
+if(localStorage.getItem('todos')){
+  result.innerHTML = localStorage.getItem('todos')
+}
